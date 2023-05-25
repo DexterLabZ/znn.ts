62ebcced574e11db4f16df93e429e617f9971822
uluCthulhu <ulu.cthulhu@proton.me>
2023-05-25 16:28:38 +0300

    another configuration for webpack

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export * from "./wallet/wallet";
+export {Api} from "./api/api";
+export {Zenon} from "./zenon";
+export {netId} from "./global";
+
+import {BlockUtils} from "./utils/block";
+const utils = {
+  BlockUtils,
+};
+export {utils};
+
+export * from "./exports";
